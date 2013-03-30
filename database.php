fac5d80b33dad330a207c75a53cceb2c7980cdd1
Łukasz Korczewski <lukasz.korczewski@gmail.com>
2013-03-30 10:47:43 +0000

    layness

--- database.php
+++ database.php
@@ -1,7 +1,9 @@
 <?php
 
+//====================================================
 // Łukasz Korczewski
 // database class
+//====================================================
 
 class Database {
 	
@@ -23,22 +25,45 @@ class Database {
 		$this->database = '';
 	}
 	
+	//------------------------------------------------
+	// setting host for connection
+	//------------------------------------------------
+	
 	function set_host($host){
 		$this->host = $host;
 	}
+	
+	//------------------------------------------------
+	// setting port for connection
+	//------------------------------------------------
 	
 	function set_port($port){
 		$this->port = $port;
 	}
 	
-	function set_user($user, $password){
+	//------------------------------------------------
+	// setting username for connection
+	//------------------------------------------------
+	
+	function set_user($user, $password = ''){
 		$this->user = $user;
+		if($password != ''){
+			$this->password = $password;
+		}
+	}
+	
+	//------------------------------------------------
+	// setting password for connection
+	//------------------------------------------------
+	
+	function set_password($password){
 		$this->password = $password;
 	}
 	
 	//------------------------------------------------
 	// setting database name for connection
 	//------------------------------------------------
+	
 	function set_database($database){
 		$this->database = $database;
 	}
@@ -46,9 +71,8 @@ class Database {
 	//------------------------------------------------
 	// establishing connection
 	//------------------------------------------------
+	
 	function connect(){
-		
-		echo '<pre>'.print_r($this, true).'</pre>';
 		
 		$this->mysqli = mysqli_connect(
 			$this->host,
@@ -69,12 +93,16 @@ class Database {
 		$this->state = 1;
 		return true;
 	}
+
+	//------------------------------------------------
+	// executing query
+	//------------------------------------------------
 	
 	function query($query){
 		
 		// connection not initialized
 		if($this->state == 0) {
-			return false;
+			$this->connect();
 		}
 		
 		// query
@@ -92,7 +120,10 @@ class Database {
 		return $array_result;
 	}
 	
+	//------------------------------------------------
 	// closing connection
+	//------------------------------------------------
+	
 	function close(){
 		mysqli_close($this->mysqli);
 	}
